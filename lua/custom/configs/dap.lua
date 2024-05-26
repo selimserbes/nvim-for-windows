1fe028fceca0cd624a300208c515a2e0869a78cf
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 19:45:50 +0300

    dap edited for js and ts debugger

--- lua/custom/configs/dap.lua
+++ lua/custom/configs/dap.lua
@@ -44,5 +44,24 @@ dap.configurations.rust = {
   },
 }
 
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = 8123,
+  executable = {
+    command = "js-debug-adapter.cmd",
+  },
+}
 
+for _, language in ipairs { "typescript", "javascript" } do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+      runtimeExecutable = "node",
+    },
+  }
 end
