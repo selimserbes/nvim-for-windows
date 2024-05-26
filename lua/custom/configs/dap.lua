3fbe5e8e833dd702130358f1067257879110989d
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 19:45:33 +0300

    dap edited for rust debugger

--- lua/custom/configs/dap.lua
+++ lua/custom/configs/dap.lua
@@ -20,7 +20,29 @@ dap.configurations.go = {
   },
 }
 
+dap.adapters.codelldb = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = extension_path .. "codelldb\\extension\\adapter\\codelldb.exe",
+    args = { "--port", "${port}" },
+    -- On windows you may have to uncomment this:
+    detached = false,
+  },
+}
 
+dap.configurations.rust = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+}
 
 
 end
