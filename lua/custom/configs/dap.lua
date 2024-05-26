482ae28f283e9e5d07be51dbd802917d5b370ccd
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 19:45:12 +0300

    dap edited for go debugger

--- lua/custom/configs/dap.lua
+++ lua/custom/configs/dap.lua
@@ -1,4 +1,26 @@
 local dap = require "dap"
 local extension_path = vim.env.HOME .. "\\AppData\\Local\\nvim-data\\mason\\packages\\"
 
+dap.adapters.delve = {
+  type = "server",
+  port = "${port}",
+  executable = {
+    command = extension_path .. "delve\\dlv.exe",
+    args = { "dap", "-l", "127.0.0.1:${port}" },
+    -- add this if on windows, otherwise server won't open successfully
+    detached = false,
+  },
+}
+dap.configurations.go = {
+  {
+    type = "delve",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+}
+
+
+
+
 end
