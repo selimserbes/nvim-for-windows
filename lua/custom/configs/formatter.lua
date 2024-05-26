b4b800088b6e3bbb2109a20b0e4df313b3f4f45f
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 19:46:51 +0300

    formatter edited for rust, go, python, js and ts

--- lua/custom/configs/formatter.lua
+++ lua/custom/configs/formatter.lua
@@ -1,2 +1,27 @@
 local conform = require "conform"
 
+conform.setup {
+  formatters_by_ft = {
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    svelte = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    markdown = { "prettier" },
+    graphql = { "prettier" },
+    lua = { "stylua" },
+    python = { "ruff", "black", "isort" },
+    rust = { "rustfmt" },
+    go = { "gofumpt", "goimports_reviser", "golines" },
+  },
+
+  format_on_save = {
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  },
+}
