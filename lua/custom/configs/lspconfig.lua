715eaf22480b76e5f19b41c09f1a3ed55c96563d
selimserbes <omnicoder.ceng@gmail.com>
2024-05-26 19:47:56 +0300

    lspconfig edited for rust

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,6 +4,19 @@ local capabilities = config.capabilities
 local util = require "lspconfig/util"
 local lspconfig = require "lspconfig"
 
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  root_dir = util.root_pattern "Cargo.toml",
+  settings = {
+    ["rust_analyzer"] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+}
 
 
 
